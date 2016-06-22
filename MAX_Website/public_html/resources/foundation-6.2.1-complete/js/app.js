4b800d5cfd717db195ebb7e9ca76708ab585af21
Andrew Clawson <andrewjclawson18@gmail.com>
2016-06-22 09:33:34 -0600

    index.html menu options updated colors adjusted in app.css.

--- MAX_Website/public_html/resources/foundation-6.2.1-complete/js/app.js
+++ MAX_Website/public_html/resources/foundation-6.2.1-complete/js/app.js
@@ -1,4 +1,8 @@
-$(document).foundation()
-
-
+$(document).foundation();
+$("#menu").toggleClass("is-open");
+/*
+$(".toggle-menu").on("click", function(){
+            $("#menu").toggleClass("is-open");
+       });
+       */
 
